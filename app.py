f35f32f9e5dfa3a3223c46603d4175a3823861ed
Rupesh Mishra <mrupesh060@gmail.com>
2026-04-07 01:21:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,24 @@ import streamlit as st
 import joblib
 import numpy as np
 
+# Load model
 model = joblib.load("knn_model.pkl")
 
+# Title
 st.title("Iris Flower Prediction")
 
+# Inputs
 sl = st.number_input("Sepal Length")
 sw = st.number_input("Sepal Width")
 pl = st.number_input("Petal Length")
 pw = st.number_input("Petal Width")
 
+# Prediction
 if st.button("Predict"):
     data = np.array([[sl, sw, pl, pw]])
-    pred = model.predict(data)
-
-    labels = ["Setosa", "Versicolor", "Virginica"]
-    st.write("Prediction:", labels[pred[0]])
+    
+    # Get prediction
+    prediction = model.predict(data)[0]
+    
+    # Show result
+    st.success(f"Prediction: {prediction}")
